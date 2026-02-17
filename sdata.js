772bd142443fed5c477ceeb2b7dcead534d59dd6
Wesley Clifford <wesley.clifford@gmail.com>
2026-02-17 00:11:38 -0500

    Changing initial message

--- sdata.js
+++ sdata.js
@@ -1,6 +1,6 @@
 export let sData = 
 {
-    tut0: "<p>One day, you're walking through the woods - outside of town but not too far - wondering how to get treasure without messing around with all that troublesome <i>adventuring</i>.</p><p>Suddenly, a quantum doorway appears on the path beside you. You could go in there; surely there's treasure within.</p><p>But also... you know... <i>adventure</i>.</p><p>Suddenly, someone else turns the corner of the path and sees you standing next to the quantum doorway.</p><p>&quot;You, there!&quot; he shouts, &quot;is that your quantum doorway? I've been aching to go on an <i>adventure</i>! Tell you what, if you let me go in there, we can share whatever I find 50/50!&quot;</p>",
+    tut0: "<p>Welcome to the Multiverse Mining Company!</p><p>As the year 2100 dawns, MMC is expanding its role into your Universe, and we're excited to offer YOU fantastic business opportunities!</p><p>Everything you need to explore and exploit your brand new Universe is in this device.</p>",
     
     endofthefile: "End of the file!"
 }
