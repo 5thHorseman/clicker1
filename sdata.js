02c9d0a026329915c65e1540607e8a31233ca475
Wesley Clifford <wesley.clifford@gmail.com>
2026-02-19 02:38:15 -0500

    bugfix

--- sdata.js
+++ sdata.js
@@ -2,7 +2,7 @@ export let sData =
 {
     tut0: "<p>Welcome to the Multiverse Mining Company!</p><p>As the year 2100 dawns, MMC is expanding its role into your Universe, and we're excited to offer YOU fantastic business opportunities!</p><p>Everything you need to explore and exploit your copy of Earth is in this device.</p><p>So explore, gather, build, whatever you feel like. You can't screw up, and if you screw it up anyway you an always start over in a different Universe. There are an infinite number of them after all!",
     
-    tut11: "<p>Woah!</p>"
+    tut1: "<p>Woah!</p>"
 
     endofthefile: "End of the file!"
 }
